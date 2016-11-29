473b8f6b32ac821ed50d7e0a4994bf0e8a7021a6
Mitchell Hudson <mitchellhudson@mitchells-MacBook-Pro.local>
2016-11-29 11:41:53 -0800

    update

--- class-6/Weather-2/main.js
+++ class-6/Weather-2/main.js
@@ -1,24 +1,12 @@
-// Get references to all of the elements that display info
-var coords = $("#coords");
-var weatherMain = $("#weather-main");
-var desc = $("#desc");
-var icon = $("#icon");
-var temp = $("#temp");
-var temp_min = $("#temp_min");
-var temp_max = $("#temp_max");
-var pressure = $("#pressure");
-var humidity = $("#humidity");
-var speed = $("#speed");
-var clouds = $("#clouds");
-var dt = $("#dt");
-var sunrise = $("#sunrise");
-var sunset = $("#sunset");
-var locationName = $("#location-name");
+// Register with openweathermap and get your API key.
+var apikey = "2854c5771899ff92cd962dd7ad58e7b0";
 
-var cityForm = $("#city-form");
-var cityInput = $("#city-input");
-var saveCityButton = $("#save-city-button");
-
+// -----------------------------------------------------------------------
+// 
+// Initialize the app
+// 
+// -----------------------------------------------------------------------
+// 
 // Load weather for the city saved in local storage, if there is a one...
 var savedCity = getCity();
 // Check this city if nothing was saved it should be undefined.
@@ -31,78 +19,92 @@ if (savedCity != undefined) {
     $("body").addClass("no-weather");
 }
 
+// ----------------------------------------------------------------------
+
+
+
+
+// ----------------------------------------------------------------------
+// 
+// Load weather for city
+// 
+// ----------------------------------------------------------------------
 // Call this method with the city name to load weather for that city
 function loadData(city) {
-    // Register and get an api key
-    var apikey = "2854c5771899ff92cd962dd7ad58e7b0";
     // Make a path with the city and api key
     var path = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey;
-
+    
     // Use jQuery to load JSON data.
     $.get(path, function (data) {
         // Print the data to console. Go look at it right now!
         console.log(data);
-
+        
         // Check for errors
         if (data.cod == 200) {
+            // COD = 200 and everything is okay.
             $("body").removeClass("no-weather");
         } else {
+            // Otherwise there was a problem hide UI stuff.
             $("body").addClass("no-weather");
             return
         }
 
-
         // Collect values from the json data and display it in each of the divs above.
-        // coords.html(data.coord.lat + " " + data.coord.lon);
         $("#lat").html(data.coord.lat);
         $("#lon").html(data.coord.lon);
 
         // data.weather array sometimes has more than one item!
-        weatherMain.html(data.weather[0].main);
-        desc.html(data.weather[0].description);
+        $("#weather-main").html(data.weather[0].main);
+        $("#desc").html(data.weather[0].description);
         // * Use the icon name to load an image for the weather.
-        icon.html("<img src='weather-icons/" + data.weather[0].icon + ".svg'>");
+        $("#icon").html("<img src='weather-icons/" + data.weather[0].icon + ".svg'>");
         // For more info on icons and condition codes: https://openweathermap.org/weather-conditions
 
         // * Convert the temp from Kelvin to F or C.
-        temp.html(kToF(data.main.temp, 0));
+        $("#temp").html(kToF(data.main.temp, 0));
 
         // * Convert these from K to T or C.
-        temp_min.html(kToF(data.main.temp_min, 0));
-        temp_max.html(kToF(data.main.temp_max, 0));
+        $("#temp_min").html(kToF(data.main.temp_min, 0));
+        $("#temp_max").html(kToF(data.main.temp_max, 0));
 
-        pressure.html(data.main.pressure);
-        humidity.html(data.main.humidity + "%");
+        $("#pressure").html(data.main.pressure);
+        $("#humidity").html(data.main.humidity + "%");
 
         // Wind - These properties are some times missing. Check for undefined before displaying them!
         var windSpeed = data.wind.speed;
         var windDeg = data.wind.deg;
         var windGust = data.wind.gust;
+        
+        var wind = windSpeed + " mph";
+        if (windDeg != undefined) {wind += " "+windDeg+"&deg;";}
+        if (windGust != undefined) {wind += " "+ windGust+" Gust"}
 
-        speed.html(windSpeed);
+        $("#speed").html(wind);
 
-        clouds.html(data.clouds.all);
-        dt.html(new Date(data.dt * 1000).toDateString());
-        sunrise.html(formatAMPM(new Date(data.sys.sunrise * 1000)));
-        sunset.html(formatAMPM(new Date(data.sys.sunset * 1000)));
-        locationName.html(data.name);
+        $("#clouds").html(data.clouds.all);
+        $("#dt").html(new Date(data.dt * 1000).toDateString());
+        $("#sunrise").html(formatAMPM(new Date(data.sys.sunrise * 1000)));
+        $("#sunset").html(formatAMPM(new Date(data.sys.sunset * 1000)));
+        $("#location-name").html(data.name);
 
         // Set background graient based on temp
         // var backgroundCSS = generateBackground(data.main.temp_min, data.main.temp_max, data.main.temp);
         // $("body").attr("style", backgroundCSS);
         
         // ***** Load five day forecast *****
+        // Now that we have a location we can ask for a 5 day forecast.
         loadFiveDayForecast(data.coord.lat, data.coord.lon);
     });
 }
 
 
-
-// ********
+// ----------------------------------------------------------------------
+// 
 // Five day forecast
+// 
+// ----------------------------------------------------------------------
+// Get the five day forecast for the lat and lon
 function loadFiveDayForecast(lat, lon) {
-    //five-day-forecast
-    var apikey = "2854c5771899ff92cd962dd7ad58e7b0";
     var path = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apikey;
     $.get(path, function(data) {
         console.log("-------- Five Day --------");
@@ -113,11 +115,12 @@ function loadFiveDayForecast(lat, lon) {
         var html = "";
         for (var i = 0; i < data.list.length; i++) {
             var obj = {};
+            
             obj.clouds = data.list[i].clouds.all;
             obj.dt =  new Date(data.list[i].dt * 1000).toDateString()+" "+formatAMPM(new Date(data.list[i].dt * 1000));
-            obj.temp = data.list[i].main.temp;
-            obj.temp_min = data.list[i].main.temp_min;
-            obj.temp_max = data.list[i].main.temp_max;
+            obj.temp = kToF(data.list[i].main.temp);
+            obj.temp_min = kToF(data.list[i].main.temp_min);
+            obj.temp_max = kToF(data.list[i].main.temp_max);
             obj.grnd_level = data.list[i].main.grnd_level;
             obj.humidity = data.list[i].main.humidity;
             obj.pressure = data.list[i].main.pressure;
@@ -127,11 +130,11 @@ function loadFiveDayForecast(lat, lon) {
             obj.windSpeed = data.list[i].wind.speed;
             obj.windDeg = data.list[i].wind.deg;
             list.push(obj);
-            html += "<div><span>"+
+            html += "<div class='three-hr'><span>"+
                 obj.dt+"</span> <span>"+
-                obj.temp+"</span> <span>"+
-                obj.humidity+"</span> <span>"+
-                obj.description+"</span></div>";
+                obj.temp+"&deg;</span> <div><span>Humidity: "+
+                obj.humidity+"%</span> <span>"+
+                obj.description+"</span></div></div>";
         } 
         
         $("#five-day-forecast").html(html);
@@ -139,49 +142,67 @@ function loadFiveDayForecast(lat, lon) {
 }
 
 
-// Form
 
+// ------------------------------------------------------------------
+//
+// City Form and buttons
+// 
+// ------------------------------------------------------------------
+
+// Handle submit of city form
 $("#city-form").submit(function (event) {
     event.preventDefault();
     console.log("City form Submit");
-    var city = cityInput.val();
+    var city = $("#city-input").val();
     $(".city-form-container").removeClass("show");
-    // Show loading progress??
     loadData(city);
 });
 
-saveCityButton.click(function (event) {
+// Handle the Save city button (looks like a heart)
+$("#save-city-button").click(function (event) {
     event.preventDefault();
-    var city = cityInput.val();
+    var city = $("#city-input").val();
     saveCity(city);
 });
 
-// Save city to local storage
-
-function saveCity(cityName) {
-    localStorage.setItem("weather-app", cityName);
-}
-
-// !!! This possibly returns null you must handle this!
-function getCity() {
-    return localStorage.getItem("weather-app");
-}
-
-
-// Buttons
-
+// Handle the location button
 $("#location-button").click(function () {
     $(".city-form-container").addClass("show");
 });
-
-$("#save-city-button").click(function () {
-    // $(".city-form-container").addClass("show");
-});
+// ------------------------------------------------------------------
 
 
+
+
+// ------------------------------------------------------------------
+// 
+// Save city to local storage
+// 
+// ------------------------------------------------------------------
+
+function saveCity(cityName) {
+    // Save the cityName to local storage with the key: weather-app
+    localStorage.setItem("weather-app", cityName);
+}
+
+// Get the city name from local starage for the key: weather-app
+function getCity() {
+    // !!! This possibly returns null you must handle this!
+    return localStorage.getItem("weather-app");
+}
+// ---------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------
+// 
 // Helper functions
+// 
+// --------------------------------------------------------------
 
-
+// Adds a 0 in front of a digit if it is a single digit. 
+// Use this for writing the time as 09:03
 function padWithZero(i) {
     if (i < 10) {
         i = "0" + i;
@@ -189,6 +210,9 @@ function padWithZero(i) {
     return i;
 }
 
+
+// Get the time formatted from a date object. 
+// 06:05:09
 function getTimeFrom(date) {
     var h = padWithZero(date.getHours());
     var m = padWithZero(date.getMinutes());
@@ -196,6 +220,8 @@ function getTimeFrom(date) {
     return h + ":" + m + ":" + s;
 }
 
+// Get the formatted time for a date
+// 09:08:05 am
 function formatAMPM(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
@@ -207,23 +233,44 @@ function formatAMPM(date) {
     return strTime;
 }
 
-
-
+// Convert temp kelvin to Fahrenheit
 function kToF(t, decimals) {
     // Do some math and round to two decimal places.
     return (t * 9 / 5 - 459.67).toFixed(decimals);
 }
 
+// Convert temp Kelvin to Celsius 
 function kToC(t, decimals) {
     return (t - 273.15).toFixed(decimals);
 }
 
+
+//******************************************
 // Prevent scroll on mobile
-$(document).on('touchmove', function (e) {
-    e.preventDefault();
+$(document).on('touchmove', function(event) {
+    event.preventDefault();
+    console.log("touch move");
+});
+
+$(document).on('touchstart', function(event){
+    console.log("touch start");
+});
+
+$(document).on('touchend', function(event){
+    console.log("touch end!");
+    
 });
 
 
+
+
+
+
+
+
+
+// ****************************************************************************
+// Unsuccesful attempt to convert temp to color values and generate a gradient. 
 
 // https://gist.github.com/paulkaplan/5184275
 // From http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
